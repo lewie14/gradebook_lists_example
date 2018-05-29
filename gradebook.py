50c870a0dca46c55ddcb5fd6486832b3835d877e
lewie14 <lewie@netvigator.com>
2018-05-29 15:23:51 +0800

    Zipped lists and test print out

--- gradebook.py
+++ gradebook.py
@@ -4,7 +4,11 @@
 last_semester_gradebook = [("politics", 80), ("latin", 96), ("dance", 97), ("architecture", 65)]
 
 #Classes taken
-subjects = ["Physics", "Calculus", "Poetry", "history"]
+subjects = ["Physics", "Calculus", "Poetry", "History"]
 
 #Grades for these subjects
 grades = [ 98, 97, 85, 88]
+
+gradebook = zip(subjects, grades)
+
+print(list(gradebook))
