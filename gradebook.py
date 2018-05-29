52732811adaaac1e0c234080a1cf1402bea62074
lewie14 <lewie@netvigator.com>
2018-05-29 15:18:36 +0800

    First commit, set up three basic lists

---
+++ gradebook.py
@@ -0,0 +1,10 @@
+#Gradebook Example
+
+#Last terms grades
+last_semester_gradebook = [("politics", 80), ("latin", 96), ("dance", 97), ("architecture", 65)]
+
+#Classes taken
+subjects = ["Physics", "Calculus", "Poetry", "history"]
+
+#Grades for these subjects
+grades = [ 98, 97, 85, 88]
